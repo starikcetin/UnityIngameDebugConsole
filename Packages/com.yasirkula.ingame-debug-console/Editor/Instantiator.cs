0284365251ce024a59ca6c1076589cccdef19d4a
starikcetin <cetinsamedtarik@gmail.com>
2019-10-16 16:10:08 +0300

    * added an instantiator for creating the prefab from the context menu * a test scene * version bump

---
+++ Packages/com.yasirkula.ingame-debug-console/Editor/Instantiator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace IngameDebugConsole
+{
+    internal class Instantiator : ScriptableObject
+    {
+        private static Instantiator _instance;
+        private static Instantiator Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = CreateInstance<Instantiator>();
+                }
+
+                return _instance;
+            }
+        }
+
+        [SerializeField] private GameObject _prefab;
+
+        [MenuItem("GameObject/Ingame Debug Console", false, 12)]
+        private static void InstantiatePrefab()
+        {
+            Selection.activeObject = PrefabUtility.InstantiatePrefab(Instance._prefab);
+        }
+
+        [MenuItem("GameObject/Ingame Debug Console", true, 12)]
+        private static bool ValidateInstantiatePrefab()
+        {
+            var go = Instance._prefab;
+            return go != null && PrefabUtility.IsPartOfPrefabAsset(go);
+        }
+    }
+}
